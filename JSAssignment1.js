1e22d36ff25d4506e0af888f731d8538a60224a2
rolteame <r.s.adebiyi@gmail.com>
2020-09-09 14:49:02 +0100

    Assignment submissions

---
+++ JSAssignment1.js
@@ -0,0 +1,12 @@
+
+//Assignment 1a
+let value = prompt('Enter a Value');
+value = parseInt(value);
+output = (value >= 100) ? alert('Century or Above a Century') : alert('Less than a Century');
+
+//Assignment 1b
+let input = prompt('Enter input');
+if (input != null && input != undefined) {
+    alert(input);
+}
+
