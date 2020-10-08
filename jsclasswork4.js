ca1eeb8a37fe156ea9b5170b6bdc3605dfa6425f
rolteame <r.s.adebiyi@gmail.com>
2020-10-08 13:09:06 +0100

    assignment changes

--- jsclasswork4.js
+++ jsclasswork4.js
@@ -22,24 +22,25 @@ users = JSON.parse(localStorage.getItem('users'));
 if(users == null) {
     users = [];
 }
-
+var userId;
 let displayCard = () => {
     let showUser = '';
         for(let i = 0; i < users.length; i++) {
+            
             showUser += `
                 <div class="mx-auto">
-                    <img class="rounded img-fluid border border-light" style="width: 200px" src=" ${users[i].profilePics}" alt="">
-                    <div class="text-light mx-auto d-block">
+                    <img class="rounded img-fluid border border-light d-block mx-auto" style="width: 400px;" src=" ${users[i].profilePics}" alt="">
+                    <div class="text-light text-center">
                         <p>Full Name : ${users[i].fullName}</p>
                         <p>Email : ${users[i].email}</p>
                         <p>Phone Number : ${users[i].phone}</p>
                     </div>
-                    <div class="">
+                    <div class="d-block mx-auto">
                         <button id="deletebtn" class="btn btn-primary" onclick="deleteButton(${i})">Delete User</button>
                         <button class="btn btn-primary" data-toggle="modal" data-target="#addButton" onclick=editButton(${i})>Edit User</button>
                     </div> 
                 </div> 
-                <hr>
+                <hr style="background-color: white">
             `
         }
         document.getElementById('users').innerHTML = showUser;
@@ -48,7 +49,8 @@ displayCard();
 
 let newUserInput = () => {
     addProfilePics = document.getElementById('profilePics').value;
-    let newProfilePics = addProfilePics.split('//')[2];
+
+    let newProfilePics = addProfilePics.split('\\')[2];
     let newFullName = document.getElementById('fullName').value;
     let newEmail = document.getElementById('email').value;
     let newPhoneNumber = document.getElementById('phone').value;
@@ -79,13 +81,36 @@ let deleteButton = (id) => {
 
 let editButton = (id) => {
     let userDetails = users[id];
+    userId = id;
 
-    document.getElementById('profilePics').value;
+    //document.getElementById('profilePics').value = userDetails.profilePics;
     document.getElementById('fullName').value = userDetails.fullName;
     document.getElementById('email').value = userDetails.email;
     document.getElementById('phone').value = userDetails.phone;   
 }
 
-let update = (id) => {
+let update = () => {
+
+    addProfilePics = document.getElementById('profilePics').value;
+    let editedProfilePics = addProfilePics.split('\\')[2];
+    let editedFullName = document.getElementById('fullName').value;
+    let editedEmail = document.getElementById('email').value;
+    let editedPhoneNumber = document.getElementById('phone').value;
+
+    let editedUser = {
+        "profilePics" : editedProfilePics,
+        "fullName" : editedFullName,
+        "email" : editedEmail,
+        "phone" : editedPhoneNumber
+    }
+
+    users[userId] = editedUser;
+    confirm('Are you sure you want to update this user');
+    localStorage.setItem('users',JSON.stringify(users));
+    displayCard();
+    location.reload();
+}
+
+let search = () => {
     
 }
